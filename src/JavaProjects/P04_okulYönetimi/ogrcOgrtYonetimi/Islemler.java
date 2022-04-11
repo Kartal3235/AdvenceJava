bdd770f4fa0061909db87a72ecb68c755977366f
Bahadır Koramaz <bkork1maz@gmail.com>
2022-04-12 00:15:39 +0300

    12.04.22

---
+++ src/JavaProjects/P04_okulYönetimi/ogrcOgrtYonetimi/Islemler.java
@@ -0,0 +1,62 @@
+package JavaProjects.P04_okulYönetimi.ogrcOgrtYonetimi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Islemler {
+    static List<Kisi> ogrncListesi = new ArrayList<>();
+    static ArrayList<Kisi> ogrtmListesi = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
+    static String kisiTuru;
+    public static void girisPaneli() {
+        System.out.println("====================================\nOGRENCI VE OGRETMEN YONETIM PANELI\n" +
+                "====================================\n"
+                + "1- OGRENCI ISLEMLERI\n2- OGRETMEN ISLEMLERI\nQ- CIKIS");
+        System.out.print("isleminiz seciniz : ");
+        String secim = scan.next().toUpperCase();
+        switch (secim) {
+            case "1":
+                kisiTuru="OGRENCI";
+                islemMenusu();
+                break;
+            case "2":
+                kisiTuru="OGRETMEN";
+                islemMenusu();
+                break;
+            case "Q":
+                break;
+            default:
+                System.out.println("hatalı giriş yaptınız  :(");
+                girisPaneli();//recursive method call
+                break;
+        }
+    }
+    public static void islemMenusu(){
+        System.out.println("Sectiginiz  Kişi turu: "+ kisiTuru+" icin  aşağıdaki işlemlerden tercih yapınız.\r\n"
+                + "============= İŞLEMLER =============\r\n"
+                + "     1-EKLEME\r\n"
+                + "     2-ARAMA\r\n"
+                + "     3-LİSTELEME\r\n"
+                + "     4-SİLME\r\n"
+                + "     0-ANA MENU");
+        System.out.print("islem tercihinizi giriniz : ");
+        int secilenIslem = scan.nextInt();
+        switch (secilenIslem) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 0:
+                girisPaneli();
+                break;
+            default:
+                System.out.println("adam bir şey girrr :( ");
+                islemMenusu();
+                break;
+        }
+    }
+}
---
+++ src/JavaProjects/P04_okulYönetimi/ogrcOgrtYonetimi/Runner.java
@@ -0,0 +1,8 @@
+package JavaProjects.P04_okulYönetimi.ogrcOgrtYonetimi;
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+    }
+}
